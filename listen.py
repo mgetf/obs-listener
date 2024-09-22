24e7a6185228dedf923b36e01835068ca36f45c2
tommy-mor <thmorriss@gmail.com>
2024-09-22 13:11:37 -0400

    records scores

--- listen.py
+++ listen.py
@@ -1,15 +1,20 @@
 import asyncio
 import websockets
 import json
+from collections import defaultdict
 
+# [arena][1|2] = score
+scores = defaultdict(lambda: defaultdict(int))
 async def handle_websocket(websocket, path):
     try:
         async for message in websocket:
             try:
                 # Parse the incoming message as JSON
                 data = json.loads(message)
-                print("Received JSON:")
-                print(json.dumps(data, indent=2))
+                if data["type"] == "ScoreNotification":
+                    scores[data["payload"]["arena"]][1] = data["payload"]["p1Score"]
+                    scores[data["payload"]["arena"]][2] = data["payload"]["p2Score"]
+                    print(scores)
             except json.JSONDecodeError:
                 print(f"Received non-JSON message: {message}")
     except websockets.exceptions.ConnectionClosed:
